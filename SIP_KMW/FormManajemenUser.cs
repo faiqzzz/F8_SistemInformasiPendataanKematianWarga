89e45e97539c571d1acb9116b6f62e0b90dba3c2
faiqzzz <ridhofaiqzzz@gmail.com>
2026-04-20 04:04:50 +0700

    Void tampilkan data

--- SIP_KMW/FormManajemenUser.cs
+++ SIP_KMW/FormManajemenUser.cs
@@ -44,43 +44,6 @@ namespace SIP_KMW
             }
         }
 
-        private void btnSave_Click(object sender, EventArgs e)
-        {
-
-            string alamatDatabase = "Data Source=DESKTOP-DDDRHRS\\RIDHOFAIQAHMAD;Initial Catalog=DB_SIPKMW;Integrated Security=True";
-            using (SqlConnection conn = new SqlConnection(alamatDatabase))
-            {
-                try
-                {
-                    conn.Open();
-                    // Sesuaikan dengan nama kolom di tabel 'Users' kamu
-                    string query = "INSERT INTO Users (Username, Password, Role) VALUES (@user, @pass, @role)";
-                    SqlCommand cmd = new SqlCommand(query, conn);
-
-                    cmd.Parameters.AddWithValue("@user", txtUsername.Text);
-                    cmd.Parameters.AddWithValue("@pass", txtPassword.Text); // Untuk tugas, password polos dulu oke lah
-                    cmd.Parameters.AddWithValue("@role", cbRole.Text); // ComboBox isi: Administrator / Petugas
-
-                    cmd.ExecuteNonQuery();
-                    MessageBox.Show("User baru berhasil didaftarkan!");
-
-                    TampilDataUser(); // Refresh tabel user
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Gagal tambah user: " + ex.Message);
-                }
-            }
-        }
-
-        private void FormManajemenUser_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
+        
     }
 }
